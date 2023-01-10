900a9e2821a0c8d87750c9b322c079760db7986e
Facu Palavecino <facundo.palavecino@muttdata.ai>
2023-01-10 18:45:52 -0300

    PR: forecasting with prophet + adding more features (#4)
    * feat: forecasting with prophet + adding more features
    
    * feat: prophet notebook

--- src/dags/sql/create_cities_table.sql
+++ src/dags/sql/create_cities_table.sql
@@ -18,13 +18,24 @@ CREATE TABLE IF NOT EXISTS weather_measurements (
     feels_like float8,
     pressure float8,
     humidity INT,
+    rain float8,
+    snowfall float8,
+    cloudcover float8,
+    shortwave_radiation float8,
     measurement_time TIMESTAMP,
     UNIQUE(city_id, measurement_time),
     CONSTRAINT fk_city
         FOREIGN KEY(city_id) 
             REFERENCES cities(city_id)
 );
-
+comment on column weather_measurements.temperature is '	Air temperature at 2 meters above ground (ºC)';
+comment on column weather_measurements.feels_like is 'The perceived feels-like temperature';
+comment on column weather_measurements.pressure is 'Pressure at surface';
+comment on column weather_measurements.humidity is 'Relative humidity at 2 meters above ground';
+comment on column weather_measurements.rain is 'Rain of the preceding hour in millimeter';
+comment on column weather_measurements.snowfall is 'Snowfall amount of the preceding hour in centimeters';
+comment on column weather_measurements.cloudcover is 'Total cloud cover as an area fraction';
+comment on column weather_measurements.shortwave_radiation is 'Shortwave solar radiation as average of the preceding hour';
 GRANT ALL
 ON weather_measurements
 TO {{ params.user }};
